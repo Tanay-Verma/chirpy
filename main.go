d9eb44ee18610560bcc9872102dffa4a66e673d7
TanayVerma <tanay.verma98@gmail.com>
2025-01-01 21:26:05 +0530

    Add Stateful header for tracking api hit metrics.

--- main.go
+++ main.go
@@ -1,20 +1,51 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
 func main() {
-	mux := http.NewServeMux()
-	server := http.Server{
-		Handler: mux,
-		Addr:    ":8080",
+	const filepathRoot = "."
+	const port = "8080"
+
+	config := apiConfig{
+		fileserverHits: atomic.Int32{},
 	}
 
-	err := server.ListenAndServe()
-	if err != nil {
-		log.Fatalf("Server could not be started: %s", err)
-		return
+	mux := http.NewServeMux()
+	mux.Handle(
+		"/app/",
+		config.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))),
+	)
+	mux.HandleFunc("/healthz", handlerReadiness)
+	mux.HandleFunc("/metrics", config.handlerMetrics)
+	mux.HandleFunc("/reset", config.handlerReset)
+
+	server := http.Server{
+		Handler: mux,
+		Addr:    ":" + port,
 	}
+
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Fatal(server.ListenAndServe())
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())))
 }
